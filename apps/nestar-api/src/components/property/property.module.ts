55f460299d977d84e178ed3b475fa7ed6735b011
Asadbek Abdumalikov <abdumalikovfourth@gmail.com>
2026-08-28 19:10:08 +0900

    feat: develop createProperty GraphQL api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -5,10 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import PropertySchema from '../../schemas/Property.model';
+import { MemberModule } from '../member/member.module';
 
 @Module({
     imports: [
-         MongooseModule.forFeature([{name: "Property", schema: PropertySchema}]), AuthModule, ViewModule
+        MongooseModule.forFeature([{name: "Property", schema: PropertySchema}]), AuthModule, ViewModule, MemberModule
     ],
     providers: [PropertyResolver, PropertyService]
 })
