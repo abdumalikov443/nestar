f2be498c592da47cb7d4b959192a712d668a69e8
Asadbek Abdumalikov <abdumalikovfourth@gmail.com>
2026-09-02 17:46:11 +0900

    feat: integrate comment module related DTO

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,17 +1,17 @@
 import { ObjectId } from "bson";
-
-export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"];
-export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews"];
-
-export const availableOptions = ["propertyBarter", "propertyRent"];
-export const availablePropertySorts = ["createdAt", "updatedAt", "propertyLikes", "propertyViews", "propertyRank", "propertyPrice"];
-
-
-export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews']
- /** IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
+
+export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"];
+export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews"];
+export const availableOptions = ["propertyBarter", "propertyRent"];
+export const availablePropertySorts = ["createdAt", "updatedAt", "propertyLikes", "propertyViews", "propertyRank", "propertyPrice"];
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
+
+
+/** IMAGE CONFIGURATION **/
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
