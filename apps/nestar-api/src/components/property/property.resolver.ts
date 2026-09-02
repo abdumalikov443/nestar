cffbdfbe16af0ee2b0cacfb8cb8270e43b728464
Asadbek Abdumalikov <abdumalikovfourth@gmail.com>
2026-09-02 16:12:27 +0900

    feat: configure boardArticle module and integrate boardArticle DTO

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -77,8 +77,7 @@ export class PropertyResolver {
     @UseGuards(RolesGuard) //@ts-ignore
     @Query((returns) => Properties)
     public async getAllPropertiesByAdmin(
-        @Args('input') input: AllPropertiesInquiry,
-        @AuthMember('_id') memberId: ObjectId
+        @Args('input') input: AllPropertiesInquiry
     ): Promise<Properties> {
         console.log("Query: getAllPropertiesByAdmin");
         return await this.propertyService.getAllPropertiesByAdmin(input);
