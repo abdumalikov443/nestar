228ceb7b390f0b662bda2beb3c8332f4a0da2941
Asadbek Abdumalikov <abdumalikovfourth@gmail.com>
2026-09-03 22:25:05 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -81,6 +81,17 @@ export class MemberResolver {
         return await this.memberService.getAgents(memberId, input) as unknown as Members;
     }
 
+    @UseGuards(AuthGuard)
+    @Mutation(() => Member)
+    public async likeTargetMember(
+        @Args("memberId") input: string,
+        @AuthMember('_id') memberId: ObjectId
+    ) :Promise<Member> {
+        console.log("Mutation: likeTargetMember");
+        const likeRefId = shapeIntoMongoObjectId(input);
+        return await this.memberService.likeTargetMember(memberId, likeRefId);
+    }
+
     /** ADMIN **/
 
     @Roles(MemberType.ADMIN)
