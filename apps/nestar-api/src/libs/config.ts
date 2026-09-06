6f4fb1aa03a3bca27dd63162a27b34734ff233e9
Asadbek Abdumalikov <abdumalikovfourth@gmail.com>
2026-09-06 23:30:40 +0900

    feat: integration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,6 +1,8 @@
 import { ObjectId } from "bson";
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
+import { T } from "./types/common";
+import { pipeline } from "stream";
 
 
 export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"];
@@ -20,7 +22,39 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === "string" ? new ObjectId(target) : target;
-}
+};
+
+
+export const lookupAuthMemberLiked = (memerId: T, targetRefId: string = "$_id") => {
+	return {
+		$lookup: {
+			from: "likes",
+			let: {
+				localLikeRefId: targetRefId,
+				localMemberId: memerId,
+				localMyFavorite: true
+			},
+			pipeline: [ 
+				{
+					$match: {
+						$expr: { // likeRefId ni localLikeRefId ga teng holatini izla,  $$ => local variableni ishlatish uchun
+							$and: [{$eq: ["$likeRefId", "$$localLikeRefId"]}, {$eq: ["$memberId", "$$localMemberId"]}]
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0, // id ni olib bermaslik uchun 0 qildik
+						memberId: 1,
+						likeRefId: 1,
+						myFavorite: "$$localMyFavorite",
+					},
+				},
+			],
+			as: "meLiked",
+		},
+	};
+};
 
 export const lookupMember = {
 	$lookup: {
